5c6f6280133822001c2ed0b26bf7d7a73f359dd2
Ali <aliarjmandi339@gamil.com>
2024-07-24 09:31:36 +0330

    fix: :bug: debugging

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -12,3 +12,13 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.opt.conceallevel = 0
 	end,
 })
+
+require("neo-tree").setup({
+	filesystem = {
+		filtered_items = {
+			visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+			hide_dotfiles = false,
+			hide_gitignored = true,
+		},
+	},
+})
